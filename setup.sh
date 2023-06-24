a48bce01363029f6c59e8bc21944c8f1f3fcde38
Noah Addino <Noah.gr.addino@gmail.com>
2023-06-24 23:39:53 +0200

    addsetupscript

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if the script is run with sudo
+if [ "$EUID" -ne 0 ]; then
+   echo "This script must be run with sudo. Try: sudo ./setup.sh"
+   exit 1
+fi
+
+# Check if python3-pip is installed, if not, install it
+if ! command -v pip3 &> /dev/null; then
+    echo "python3-pip is not installed. Installing..."
+    apt-get update
+    apt-get install -y python3-pip
+else
+    echo "python3-pip is already installed."
+fi
+
+# Check if git is installed, if not, install it
+if ! command -v git &> /dev/null; then
+    echo "git is not installed. Installing..."
+    apt-get update
+    apt-get install -y git
+else
+    echo "git is already installed."
+fi
+
+# Check if USB gadget is set, if not, launch the installer
+if [ ! -f "/usr/bin/init_usb_gadget.sh" ]; then
+    echo "USB gadget is not set. Launching the installer..."
+    chmod +x ./usb_gadget_installer.sh
+    ./usb_gadget_installer.sh
+else
+    echo "USB gadget is already set."
+fi
